d14d6db7739ae4256c406e3511c5cc0294cbc2e4
Nrupen <vepatel@paypal.com>
2019-01-05 17:21:39 -0800

    new change

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
+var {mongoose} = require('./DB/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {authenticate} = require('./middleware/authenticate');
